fd8bec277ed2d179c8e95bd06442f0fd3d285995
YordaniBonilla <b.yordani93@gmail.com>
2019-07-18 17:10:01 -0500

    Included links to corresponding html documents /styled email color /deleted unnecessarry files"

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
-let a = 1;
 
-var b = 2;
 
-nombre = "Tu Eres El Elegido!!! ahora Andandooooo!"
-alert(`Hola numbero ${ a + b} ${nombre} `)
+nombre = "Bienvenido"
+alert(`Hola numbero ${nombre} `)
 
